3473331e586ccd93b82deaaafff2100f40cb0145
Jai <jaigovindmeena@gmail.com>
2026-02-26 15:53:55 +0530

    Fixed Railway dynamic PORT

--- src/server.js
+++ src/server.js
@@ -15,6 +15,8 @@ app.get("/", (req, res) => {
   res.send("Renok AI Running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
